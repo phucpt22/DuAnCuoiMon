38eed490f3ec9deb5bdad176827ac70731cd2c8b
Phạm Trọng Phúc <zzptpham231@gmail.com>
2023-04-21 22:22:16 +0700

    test

---
+++ src/main/java/com/poly/DuAnCuoiMonApplication.java
@@ -0,0 +1,13 @@
+package com.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuAnCuoiMonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuAnCuoiMonApplication.class, args);
+    }
+
+}
