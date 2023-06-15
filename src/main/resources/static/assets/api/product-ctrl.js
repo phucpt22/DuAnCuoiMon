c2907d9b8c15e5b1195422daebe53d2e0c15fece
Trọng Phúc <93562684+ToPuss@users.noreply.github.com>
2023-06-15 20:21:23 +0700

    update

--- src/main/resources/static/assets/api/product-ctrl.js
+++ src/main/resources/static/assets/api/product-ctrl.js
@@ -73,7 +73,7 @@ app.controller("product-ctrl", function($scope, $http) {
     $scope.delete = function(item) {
 
         $http
-            .delete("/rest/products/${item.id}", item)
+            .delete("/rest/products/${item.id}")
             .then((resp) => {
                 var index = $scope.items.findIndex((p) => p.id == item.id);
                 $scope.items.splice(index, 1);
@@ -102,4 +102,34 @@ app.controller("product-ctrl", function($scope, $http) {
                 console.log("Error", error);
             });
     };
+
+    $scope.pager = {
+        page: 0,
+        size:10,
+        get items(){
+            var start = this.page *this.size;
+            return $scope.items.slice(start, start + this.size);
+        },
+        get count(){
+            return Math.ceil(1.0 * $scope.items.length / this.size);
+        },
+        first(){
+            this.page = 0;
+        },
+        prev(){
+            this.page--;
+            if(this.page<0){
+                this.last();
+            }
+        },
+        next(){
+            this.page++;
+            if(this.page>=this.count){
+                this.first();
+            }
+        },
+        last(){
+            this.page = this.count-1;
+        } 
+    }
 });
