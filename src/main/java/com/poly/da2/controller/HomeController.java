02dff55ada28e92d94011333bdc208653e5116bb
Phạm Trọng Phúc <zzptpham231@gmail.com>
2023-07-16 11:17:16 +0700

    .

--- src/main/java/com/poly/da2/controller/HomeController.java
+++ src/main/java/com/poly/da2/controller/HomeController.java
@@ -1,11 +1,8 @@
 package com.poly.da2.controller;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -21,9 +18,9 @@ public class HomeController {
 
 		return "redirect:/assets/admin/index.html";
 	}
-	@RequestMapping(value = {"/homepage"})
+	@RequestMapping("/homepage")
 	public String aa() {
-		return "user/index";
+		return "index";
 	}
 
 }
